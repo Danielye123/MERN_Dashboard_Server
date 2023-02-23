85176b420fbd47931af5b1fc4a0ff031f4677257
Daniel <daniel_ye.nz@hotmail.com>
2023-02-22 20:32:44 -0800

    connect user and property routes

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import userRouter from './routes/user.routes';
+import propertyRouter from './routes/property.routes';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
-
 
 app.use(express.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
     res.send({ message: 'Hello World!' });
 })
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/properties', propertyRouter);
 
 const startServer = async () => {
     try {
